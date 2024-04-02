0f570d4cd8ea818e8ef0d647f63a1d793958d690
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-03 03:14:35 +0400

    Update 2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -4,4 +4,6 @@ a type-annotated function floor which takes a float n as argument and returns th
 
 def floor(n:float)->int: 
     """Returns the largest integer less than or"""
+    if n < 0:
+        return int(n)-1
     return int(n)
