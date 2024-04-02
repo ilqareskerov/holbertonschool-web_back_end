66d5f15f32b397191029a6d84a1588d158a3ea22
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-03 03:07:52 +0400

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,4 +4,4 @@
 
 def concat(st1: str, st2: str) -> str:
     """"that takes a string str1 and a string str2 as arguments and returns a concatenated string"""
-    return set(concat(st1, st2))
+    return concat(st1, st2)
