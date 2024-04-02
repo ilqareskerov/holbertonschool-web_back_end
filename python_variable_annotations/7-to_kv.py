59e87c21ef3dcf841a305a27882e28cf7b29d5cb
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-03 03:46:44 +0400

    Update 7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -9,4 +9,4 @@ from typing import Tuple, Union
 
 def to_kv(k : str , v : Union[float, int] ) -> Tuple[str, Union[float, int]]:    
     """returns their sum as a float"""
-    return (k, v**2)
+    return k, v**2
