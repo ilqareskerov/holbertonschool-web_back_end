58b9358485a175aac0fa7c66d0280f363be83ceb
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-03 03:02:19 +0400

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-"""
-    Module with typing Python
-"""
-
-
 def add(a: float, b: float) -> float:
     """Returns the sum of two floating-point numbers."""
     return a + b
