dd1aedff3ee06d581dbdf4f1696435604f32e7d1
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-03 03:07:19 +0400

    Create 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string"""
+
+
+def concat(st1: str, st2: str) -> str:
+    """"that takes a string str1 and a string str2 as arguments and returns a concatenated string"""
+    return set(concat(st1, st2))
