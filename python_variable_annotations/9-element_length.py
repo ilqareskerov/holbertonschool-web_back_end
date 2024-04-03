3794b3f86f038ebb239dee1ad0f0134b2776c3e3
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-04 03:40:45 +0400

    Update 9-element_length.py

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,6 +7,7 @@ argument and returns their sum as a float.
 
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Returns a list of tuples where each tuple contains an element from the input list
