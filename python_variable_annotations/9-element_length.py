319bc258f61fbcc644f9f27061eee448b3525aa5
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-04 03:41:25 +0400

    Update 9-element_length.py

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 '''
-Write a type-annotated function sum_list
-which takes a list input_list of floats as
-argument and returns their sum as a float.
+Annotate the below function’s parameters
+and return values with the appropriate types
 '''
 
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """
-    Returns a list of tuples where each tuple contains an element from the input list
-    along with its length.
-    
-    Args:
-        lst (Iterable[Sequence]): An iterable containing sequences.
-        
-    Returns:
-        List[Tuple[Sequence, int]]: A list of tuples where each tuple contains an element
-        from the input list along with its length.
-    """
+    """return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
