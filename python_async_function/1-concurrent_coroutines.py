e54d873c07e58a58de35ca60e8c408ff35b0a5a4
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-05 00:15:53 +0400

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
-""" continue at the same time witha sync """
+'''
+Test file for printing the correct output of the wait_n coroutine
+'''
+
 import asyncio
 import random
 from typing import List
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
-    """
-    float time random
-    """
-    delays: List[float] = []
-    tasks: List = []
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """list of all the delays"""
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(delayed_task)
 
-    for _ in range(n):
-        tasks.append(wait_random(max_delay))
+    for spawn in spawn_ls:
+        await spawn
 
-    for task in asyncio.as_completed((tasks)):
-        delay = await task
-        delays.append(delay)
-        
-    return delays
+    return delay_ls
