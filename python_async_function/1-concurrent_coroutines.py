a47f82e455fe8e34de498a55c47e679cd0272e7c
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-05 00:14:36 +0400

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-''' Description: asynchronous coroutine that takes in an integer argument
-                 (max_delay, with a default value of 10) named wait_random
-                 that waits for a random delay between 0 and max_delay
-                 (included and float value) seconds and eventually returns it
-    Arguments: max_delay: int = 10
-'''
-
-wait_random = __import__('0-basic_async_syntax').wait_random
+""" continue at the same time witha sync """
 import asyncio
 import random
 from typing import List
-async def wait_n(n: int =0, max_delay: int = 10) -> List[float]:
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
     float time random
     """
@@ -20,9 +18,8 @@ async def wait_n(n: int =0, max_delay: int = 10) -> List[float]:
     for _ in range(n):
         tasks.append(wait_random(max_delay))
 
-    for i in asyncio.as_completed(tasks):
-        result = await i
-        delays.append(result)
+    for task in asyncio.as_completed((tasks)):
+        delay = await task
+        delays.append(delay)
         
     return delays
-
