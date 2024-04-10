6adde029208329b34c517643afbbf704c5fd8327
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-11 02:43:02 +0400

    Ready

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,15 +4,16 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """ Class that inherits from BaseCaching and is a caching system
+        This caching system doesn’t have limit """
+
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        if key is not None and item is not None:
+        """ Assign to the dictionary """
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
