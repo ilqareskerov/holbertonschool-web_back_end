763087accb5c703ff5f5018c9f1db1dae7661731
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-11 02:42:12 +0400

    Ready

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -9,9 +9,6 @@ class BasicCache(BaseCaching):
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                keys = list(self.cache_data.keys())
-                del self.cache_data[keys[0]]
 
     def get(self, key):
         """ Get an item by key
