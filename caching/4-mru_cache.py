3929cd49b3b0d2d39717bbeae3476ee77b8e7c19
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-11 03:26:49 +0400

    Ready

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+''' Basic cache Basic cache Basic cache Basic cache
+'''
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    ''' MRU cache class
+    '''
+
+    def put(self, key, item):
+        ''' Add an item in the cache
+            and remove first if more than max items
+        '''
+        if key and item:
+            if self.cache_data.__len__() + 1 > BaseCaching.MAX_ITEMS:
+                last = self.cache_data.popitem()
+                print("DISCARD:", last[0])
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Get an item by key
+        '''
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            self.cache_data.pop(key)
+            self.cache_data[key] = value
+            return value
