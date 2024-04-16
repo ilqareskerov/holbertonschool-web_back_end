288c8f2d8051e9298b5228feac623db3e4443a10
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 06:26:59 +0400

    Ready

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -18,12 +18,14 @@ class BasicAuth(Auth):
         if authorization_header.startswith("Basic "):
             return "".join(authorization_header.split(" ")[1:])
 
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
-        ''' def extract base64 authorization header '''
-        if authorization_header is None:
-            return None
-        if type(authorization_header) != str:
-            return None
-        if authorization_header.startswith("Basic "):
-            return "".join(authorization_header.split(" ")[1:])
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        ''' def decode base 64 authorization '''
+        if base64_authorization_header and type(
+                base64_authorization_header) == str:
+            try:
+                x = base64_authorization_header.encode('utf-8')
+                base = base64.b64decode(x)
+                return base.decode('utf-8')
+            except Exception:
+                return None
