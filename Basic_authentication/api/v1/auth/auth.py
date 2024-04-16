af46037eadf296e2440b8e454848701b30e8143a
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 05:53:59 +0400

    Ready

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -3,7 +3,7 @@ from flask import request, jsonify, abort
 from typing import List, TypeVar
 
 
-class Auth():
+class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth
         """
