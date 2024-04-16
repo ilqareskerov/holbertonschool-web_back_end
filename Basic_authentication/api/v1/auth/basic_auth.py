a73f8ccdfaad5e62a5733e1dddc9de6d0ac444ee
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 06:23:20 +0400

    Ready

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,21 +3,20 @@
 manage the API authentification
 '''
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """ BasicAuth class
     """
 
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
-        ''' def decode base64 authorization header '''
-        if base64_authorization_header is None:
-            return None
-        if type(base64_authorization_header) != str:
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
             return None
         try:
-            return base64_authorization_header.encode('utf-8').decode('base64')
-        except Exception:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except base64.binascii.Error:
             return None
 
     def extract_base64_authorization_header(
