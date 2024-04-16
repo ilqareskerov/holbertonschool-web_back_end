e5ce865c1e27fd40f6195b2f67c29ea6c6acc9ef
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 06:38:59 +0400

    Ready

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 ''' Module of Basic_auth
 '''
+import base64
+from api.v1.auth.auth import Auth
 from typing import TypeVar
 from models.user import User
-from api.v1.auth.auth import Auth
-import base64
 
 
 class BasicAuth(Auth):
