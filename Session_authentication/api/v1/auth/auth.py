8076f3d4bc38e763aa2f493eccafea295ab7abd6
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-17 02:59:39 +0400

    Ready

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -1,46 +1,48 @@
 #!/usr/bin/env python3
-'''
-manage the API authentification
-'''
+""" auth module
+"""
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
-    """
-manage the API authentification
-"""
+    '''self descriptive'''
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        ''' require_authentification '''
-        if path is None:
+        '''self descriptive'''
+        if not path or not excluded_paths:
             return True
-        if excluded_paths is None:
-            return True
-        if len(excluded_paths) == 0:
-            return True
-        if path is None or excluded_paths is None:
-            return True
-        path = path + '/' if path[-1] != '/' else path
-        if path in excluded_paths:
-            return False
+
+        path += '/' if path[-1] != '/' else ''
+        wildcard = any(rex.endswith("*") for rex in excluded_paths)
+
+        if not wildcard:
+            if path in excluded_paths:
+                return False
+
+        for rex in excluded_paths:
+            if rex[-1] == '*':
+                if path.startswith(rex[:-1]):
+                    return False
+            if rex == path:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header
-        """
-        if request is None or 'Authorization' not in request.headers:
+        '''self descriptive'''
+        if request is None:
             return None
-        return request.headers['Authorization']
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user
-        """
+        '''self descriptive'''
         return None
 
     def session_cookie(self, request=None):
-        """self descriptive"""
+        '''self descriptive'''
         if not request:
             return None
+
         session_name = os.getenv("SESSION_NAME")
         return request.cookies.get(session_name)
