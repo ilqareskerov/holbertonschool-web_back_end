83e19996bf7af28522a97ac5b3bf5e4923ab9be5
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 04:06:21 +0400

    Ready

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     '''returns the SHA256 hash of a password'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''returns a boolean'''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
