d5d4606d80f58922afea86d1c06e10c2a42ea146
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 06:31:15 +0400

    Ready

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,6 +39,6 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        user = decoded_base64_authorization_header.split(':', 1)[0]
-        password = decoded_base64_authorization_header.split(':', 1)[1]
+        user = decoded_base64_authorization_header.split(':')[0]
+        password = decoded_base64_authorization_header.split(':')[1]
         return user, password
