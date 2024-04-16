928ed64fa78410b1a17b5e39232b804a095d52ff
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 06:48:34 +0400

    Ready

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-''' Module of Basic_auth
-'''
+""" Module of Basic_auth
+"""
 import base64
 from api.v1.auth.auth import Auth
 from typing import TypeVar
@@ -8,11 +8,10 @@ from models.user import User
 
 
 class BasicAuth(Auth):
-    ''' BasicAuth class
-    '''
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
-        ''' def extract base64 authorization header '''
+    """ BasicAuth class
+    """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ def extract base64 authorization header """
         if authorization_header is None:
             return None
         if type(authorization_header) != str:
@@ -20,9 +19,8 @@ class BasicAuth(Auth):
         if authorization_header.startswith("Basic "):
             return "".join(authorization_header.split(" ")[1:])
 
-    def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
-        ''' def decode base 64 authorization '''
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ def decode base 64 authorization """
         if base64_authorization_header and type(
                 base64_authorization_header) == str:
             try:
@@ -32,15 +30,14 @@ class BasicAuth(Auth):
             except Exception:
                 return None
 
-    def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> (str, str):
-        ''' return the user mail and the password '''
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """ return the user mail and the password """
         c = decoded_base64_authorization_header
         if c and type(c) == str and ":" in c:
             mail = c.split(':')[0]
             password = "".join(c.split(':', 1)[1:])
-            return(mail, password)
-        return(None, None)
+            return mail, password
+        return None, None
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """ def user object from credentials """
