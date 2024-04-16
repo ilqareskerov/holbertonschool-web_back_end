723ffeeded60d7d8f1c7f52e1b4238367f698f38
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-17 02:41:59 +0400

    Ready

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -12,9 +12,9 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ create session
-        """
-        if user_id is None or type(user_id) != str:
+        if user_id is None or not isinstance(user_id, str):
             return None
+
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
+        return session_id
