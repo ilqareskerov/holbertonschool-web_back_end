0e21c87778263ce95d667c32e769c2d7367c1837
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 05:43:31 +0400

    Ready

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -3,15 +3,23 @@
 Route module for the API
 """
 from os import getenv
+from typing import Tuple, Any
+
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(404)
