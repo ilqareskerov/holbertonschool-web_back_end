42acdc2a87d3eba3702f5d90c3434a0ee858df39
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-16 07:10:59 +0400

    Ready

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,9 +46,9 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar(User):
         """ def user object from credentials """
-        if type(user_email) != str:
+        if isinstance(user_email, str):
             return None
-        if type(user_pwd) != str:
+        if isinstance(user_pwd, str):
             return None
         if user_email and user_pwd:
             users = User.search({"email": user_email})
