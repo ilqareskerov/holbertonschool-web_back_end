9557eecbdf5d2a84e25af15840f55e16a9290120
ilqareskerov <ilqar.eskerov@mail.ru>
2024-04-23 02:12:31 +0400

    Ready

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,14 +1,18 @@
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/env python3
+''' Module user '''
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
+    ''' Class user '''
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
