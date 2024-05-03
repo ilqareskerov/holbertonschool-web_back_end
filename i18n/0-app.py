0745f0b5a78989c68b6fd15f878597e22f517252
ilqareskerov <ilqar.eskerov@mail.ru>
2024-05-04 03:55:03 +0400

    Ready

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
-"""
-basic hello world example
-"""
-import flask
+""" Basic Flask app """
 from flask import Flask, render_template
-from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
-class Config(object):
-    """
-    a configuration variable
-    """
-    LANGUAGES = ['en', 'fr']
-
-
-@app.route("/", methods=['GET'])
-def hello_world():
-    """hello world"""
+@app.route('/')
+def root():
+    """ basic Flask app """
     return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,38 +1,33 @@
 #!/usr/bin/env python3
-"""
- Parametrize templates
-"""
-import flask
-from flask import Flask, render_template, g, request
+""" Basic Flask app """
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
-    """
-    a configuration variable
-    """
+class Config:
+    """ Config """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-@babel.localeselector
-def get_locale():
-    """ if a user is logged in, use the locale from the user settings
-    """
-    user = getattr(g, 'user', None)
-    if user is not None:
-        return user.locale
-    return request.accept_languages.best_match(['en', 'fr'])
-
-
 app.config.from_object(Config)
 
 
-@app.route("/", methods=['GET'])
-def hello_world():
-    """hello world"""
+@babel.localeselector
+def get_locale():
+    """ Locale selector """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def root():
+    """ Basic Flask app """
     return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
