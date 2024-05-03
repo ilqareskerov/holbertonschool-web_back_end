885ff51d7ce11257daee2f380168353cfb7eecf9
ilqareskerov <ilqar.eskerov@mail.ru>
2024-05-04 03:46:32 +0400

    Ready

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
-''' module to learn i18n
-'''
+"""
+basic hello world example
+"""
+import flask
 from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
-@app.route('/')
-def index():
-    ''' returns index.html file
-    '''
+class Config(object):
+    """
+    a configuration variable
+    """
+    LANGUAGES = ['en', 'fr']
+
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    """hello world"""
     return render_template('0-index.html')
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
