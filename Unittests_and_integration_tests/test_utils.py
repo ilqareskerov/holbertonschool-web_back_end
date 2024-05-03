f91e1a30edb5f47a4c0014870500a7cf2a7f89b3
ilqareskerov <ilqar.eskerov@mail.ru>
2024-05-03 11:26:15 +0400

    Ready

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,14 +1,32 @@
+#!/usr/bin/env python3
+"""
+unittest class to asure that
+Access nested map function
+works as expected
+"""
+import utils
+from utils import access_nested_map, get_json, memoize
 import unittest
-from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch
+from parameterized import parameterized, parameterized_class
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    the test access nested map
+    test cases class.
+    """
+
     @parameterized.expand([
-        ({}, ("a",), "Key 'a' not found in the map"),
-        ({"a": 1}, ("a", "b"), "Key 'b' not found in the map"),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_message):
-        with self.assertRaises(KeyError) as cm:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), expected_message)
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Assert the output is
+        equal to the expected result.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+#%%
