4819a0903debfe27e933713980c3f317a9632061
ilqareskerov <ilqar.eskerov@mail.ru>
2024-05-03 11:31:12 +0400

    Ready

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python3
-"""
-unittest class to asure that
-Access nested map function
-works as expected
-"""
-import utils
+"""Unittests for utils.py"""
+from unittest import TestCase, mock
+from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
-import unittest
-from unittest.mock import patch
-from parameterized import parameterized, parameterized_class
+from typing import Any, Mapping, Sequence
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """
-    the test access nested map
-    test cases class.
-    """
+class TestAccessNestedMap(TestCase):
+    """TestAccessNestedMap class"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Assert the output is
-        equal to the expected result.
-        """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
-#%%
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected_result: Any) -> None:
+        """Test access_nested_map function"""
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
