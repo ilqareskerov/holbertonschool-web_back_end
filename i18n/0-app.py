67d196f750ee7d86c30d9cd6ed94081b31edb670
ilqareskerov <ilqar.eskerov@mail.ru>
2024-05-05 15:15:32 +0400

    Ready

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-"""
-Welcome to Holberton
-"""
+''' module to learn i18n
+'''
 from flask import Flask, render_template
-from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
-@app.route("/", methods=['GET'])
-def helloWorld():
-    """
-    Hello world
-    """
+@app.route('/')
+def index():
+    ''' returns index.html file
+    '''
     return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
