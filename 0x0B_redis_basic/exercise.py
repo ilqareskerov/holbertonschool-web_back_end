3dd5a15806e9e69663afd361d3fa5ba9e22b3ae6
ilqareskerov <ilqar.eskerov@mail.ru>
2024-05-18 01:18:54 +0400

    Ready

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,102 +1,81 @@
 #!/usr/bin/env python3
-'''
-Redis basic
-'''
+"This is a line of text"
 import redis
-import uuid
-from typing import Union, Optional, Callable
 from functools import wraps
+from typing import Union, Optional, Callable
+from uuid import uuid4
 
 
 def count_calls(method: Callable) -> Callable:
-    '''
-    Count the number of times a method is called
-    '''
-    key = method.__qualname__
+    "This is a line of text"
 
     @wraps(method)
-    def wrapper(self, *args, **kwds):
-        '''
-        Wrapper function
-        '''
+    def timesCalled(self, *args, **kwds):
+        "This is a line of text"
+        key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwds)
 
-    return wrapper
+    return timesCalled
 
 
 def call_history(method: Callable) -> Callable:
-    '''
-    Store the history of inputs and outputs
-    '''
+    "This is a line of text"
 
     @wraps(method)
-    def wrapper(self, *args, **kwds):
-        '''
-        Wrapper function
-        '''
-        self._redis.rpush(method.__qualname__ + ':inputs', str(args))
+    def history(self, *args, **kwds):
+        "This is a line of text"
+        inputs = method.__qualname__ + ":inputs"
+        outputs = method.__qualname__ + ":outputs"
+        self._redis.rpush(inputs, str(args))
         output = method(self, *args, **kwds)
-        self._redis.rpush(method.__qualname__ + ':outputs', output)
+        self._redis.rpush(outputs, output)
         return output
 
-    return wrapper
+    return history
 
 
 def replay(method: Callable):
-    '''
-    Display the history of calls of a particular function
-    '''
-    r = redis.Redis()
-    name = method.__qualname__
-    count = r.get(name).decode('utf-8')
-    inputs = r.lrange(name + ':inputs', 0, -1)
-    outputs = r.lrange(name + ':outputs', 0, -1)
-    print('{} was called {} times:'.format(name, count))
-    for i, o in zip(inputs, outputs):
-        print('{}(*{}) -> {}'.format(name, i.decode('utf-8'),
-                                     o.decode('utf-8')))
+    "This is a line of text"
+    qualname = method.__qualname__
+    redis = method.__self__._redis
+    count = redis.llen(qualname + ":inputs")
+    inputs = redis.lrange(qualname + ":inputs", 0, -1)
+    outputs = redis.lrange(qualname + ":outputs", 0, -1)
+    print(f"{qualname} was called {count} times:")
+    for input, output in zip(inputs, outputs):
+        key = input.decode("utf-8")
+        value = output.decode("utf-8")
+        print(f"{qualname}(*{key})" + f" -> {value}")
 
 
 class Cache:
-    '''
-    Cache class
-    '''
+    "This is a line of text"
 
     def __init__(self):
-        '''
-        Store an instance of Redis and flush it
-        '''
+        "This is a line of text"
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''
-        Store data using a random key
-        '''
-        key = str(uuid.uuid4())
+        "This is a line of text"
+        key = str(uuid4())
         self._redis.mset({key: data})
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
-        str, bytes, int, float]:
-        '''
-        Convert data to the desired format
-        '''
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[str, bytes, int, float]:
+        "This is a line of text"
         if fn:
             return fn(self._redis.get(key))
         return self._redis.get(key)
 
-    def get_str(self, data: str) -> str:
-        '''
-        Convert data to string
-        '''
-        return self._redis.get(data).decode('utf-8')
+    def get_str(self, key: bytes) -> str:
+        "This is a line of text"
+        return self.get(key, str)
 
-    def get_int(self, data: str) -> int:
-        '''
-        Convert data to int
-        '''
-        return int(self._redis.get(data))
+    def get_int(self, key: str) -> int:
+        "This is a line of text"
+        return self.get(key, int)
