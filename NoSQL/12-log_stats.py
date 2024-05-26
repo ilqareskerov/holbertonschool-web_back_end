eb149971422d4dbc5aa618386f9d00e7c162198b
ilqareskerov <ilqar.eskerov@mail.ru>
2024-05-26 22:26:17 +0400

    Ready

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-'''
-Provide stats about Nginx logs stored in MongoDB
-'''
+"""Print info in a collection"""
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    """ Make a checkk for all elements in a collention """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
 
-if __name__ == '__main__':
-    c = MongoClient('mongodb://localhost:27017').logs.nginx
-    print(f'{c.count_documents({})} logs')
-    print('Methods:')
-    print(f'\tmethod GET: {c.count_documents({"method": "GET"})}')
-    print(f'\tmethod POST: {c.count_documents({"method": "POST"})}')
-    print(f'\tmethod PUT: {c.count_documents({"method": "PUT"})}')
-    print(f'\tmethod PATCH: {c.count_documents({"method": "PATCH"})}')
-    print(f'\tmethod DELETE: {c.count_documents({"method": "DELETE"})}')
-    print(f'{c.count_documents({"path": "/status"})} status check')
+    print(f"{collection.estimated_document_count()} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
+
+    check_get = collection.count_documents({
+        'method': 'GET', 'path': "/status"
+    })
+    print(f"{check_get} status check")
