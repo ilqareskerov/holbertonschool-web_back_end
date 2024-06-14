df7fae4b218bebd00fe4b14b9a59e80d30d726c3
ilqareskerov <ilqar.eskerov@mail.ru>
2024-06-14 05:32:07 +0400

    Ready

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-    // returns desired student by location
     return students.filter((student) => student.location === city);
   }
-
